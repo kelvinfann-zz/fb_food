a892d2a2ecce6ddf88314f47928305e807578bd4
Kelvin Fann <kelvinfann@gmail.com>
2015-04-24 06:13:58 -0700

    All basic functionality done

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^v/$', views.vendors, name='vendors'),
+	url(r'^v/(?P<vendor_id>[0-9]+)/$', views.vendor, name='vendor'),
 	url(r'^u/$', views.events, name='events'),
-	url(r'^u/get/$', views.get_events, name='get_events'),
+	url(r'^e/(?P<event_id>[0-9]+)/$', views.event, name='event'),
+	url(r'^e/get/$', views.get_events, name='get_events'),
 ]
 
--- events/views.py
+++ events/views.py
@@ -16,12 +16,30 @@ def vendors(request):
 	}
 	return render(request, 'events/vendors.html', context)
 
+def vendor(request, vendor_id):
+	vendor = Vendor.objects.get(id=vendor_id)
+	vendor_events = vendor.events.all()
+	context = {
+		'vendor': vendor,
+		'vendor_events': vendor_events,
+	}
+	return render(request, 'events/vendor.html', context)
+
 def events(request):
 	events = Event.objects.order_by('start_time')
 	context = {
 		'events': events
 	}		
 	return render(request, 'events/events.html', context)
+
+def event(request, event_id):
+	event = Event.objects.get(id=event_id)
+	event_vendors = event.vendor_set.all()
+	context = {
+		'event': event,
+		'event_vendors': event_vendors,
+	}
+	return render(request, 'events/event.html', context)
 
 def get_events(request):
 	events = get_upcoming_events()
